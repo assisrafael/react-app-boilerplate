62472970a488248666adff0e59e5a3c2b3da7486
Igor Rafael <igor.rafael@gmail.com>
2021-07-09 23:04:24 -0300

    react strict mode

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -2,4 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 
-ReactDOM.hydrate(<App />, document.getElementById("root"));
+ReactDOM.hydrate(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
