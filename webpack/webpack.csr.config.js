assisrafael/react-app-boilerplate

commit 62c9c09b7f77c6b4b71e3b6c7e896045020cc26d

chore: improve configuration with .env files

webpack/webpack.csr.config.js
@@ -11,6 +11,7 @@ const {
   defaultJsRule,
   commonConfig,
 } = require("./webpack.common.config");
+const { CSR_PORT, SSR_PORT } = require("../config");
 
 if (isDevelopment) {
   //enable SSR with fast-refresh
@@ -38,7 +39,7 @@ module.exports = {
   },
   devtool: false,
   devServer: {
-    port: 3000,
+    port: CSR_PORT,
     writeToDisk: true,
     clientLogLevel: "debug",
     hot: true,
@@ -50,7 +51,7 @@ module.exports = {
     proxy: [
       {
         context: ["/**", "!.js"],
-        target: "http://localhost:9000",
+        target: `http://localhost:${SSR_PORT}`,
         logLevel: "debug",
       },
     ],

.gitignore
@@ -1,3 +1,4 @@
 dist
 node_modules
 .nvmrc
+*.env

config/index.js
@@ -1,0 +1,11 @@
+"use strict";
+
+require("dotenv").config();
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+process.env.CSR_PORT = process.env.CSR_PORT || 3000;
+process.env.SSR_PORT = process.env.SSR_PORT || 9000;
+
+exports.isDevelopment = process.env.NODE_ENV !== "production";
+exports.SSR_PORT = process.env.SSR_PORT;
+exports.CSR_PORT = process.env.CSR_PORT;

src/ssr/renderSSR.js
@@ -18,12 +18,8 @@ exports.renderSSR = async function renderSSR({ url }) {
     await reloadSSRBundle();
   }
 
-  const { html, render } = bundle;
-
-  const partialHtml = render({ url, context });
-
   return {
-    html: html.replace('<div id="root">', `<div id="root">${partialHtml}`),
+    html: renderHtml({ url, context }),
     context: context,
   };
 };
@@ -38,4 +34,12 @@ async function reloadSSRBundle() {
 
   bundle.html = html;
   bundle.render = render;
+}
+
+function renderHtml({ url, context }) {
+  const { html, render } = bundle;
+
+  const partialHtml = render({ url, context });
+
+  return html.replace('<div id="root">', `<div id="root">${partialHtml}`);
 }

src/ssr/server.js
@@ -2,15 +2,15 @@
 
 const express = require("express");
 const morgan = require("morgan");
+const { SSR_PORT } = require("../../config");
 
 const { renderSSR, reloadSSRBundle } = require("./renderSSR");
 
-const PORT = process.env.PORT || 9000;
 const server = express();
 
 server.use(morgan("dev"));
 
-server.use("/invalidate-ssr", (req, res) => {
+server.use("/api/ssr/invalidate", (req, res) => {
   reloadSSRBundle();
   res.sendStatus(200);
 });
@@ -26,15 +26,15 @@ server.use("/**", async (req, res, next) => {
         res.writeHead(301, {
           Location: context.url,
         });
-        res.end();
       } else {
+        res.set("Content-Type", "text/html");
         res.write(html);
-        res.end();
       }
+      res.end();
     })
     .catch(next);
 });
 
-server.listen(PORT, () => {
-  console.log("server listening on port ", PORT);
+server.listen(SSR_PORT, () => {
+  console.log("server listening on port ", SSR_PORT);
 });

webpack/invalidateSSRCache.js
@@ -2,14 +2,16 @@
 
 const http = require("http");
 
+const { SSR_PORT } = require("../config");
+
 exports.invalidateSSRCache = function invalidateSSRCache() {
   setTimeout(() => {
     console.log("!! Invalidating SSR cache");
     const req = http.request(
       {
         hostname: "localhost",
-        port: 9000,
-        path: "/invalidate-ssr",
+        port: SSR_PORT,
+        path: "/api/ssr/invalidate",
         method: "GET",
       },
       (res) => {

webpack/webpack.common.config.js
@@ -1,12 +1,10 @@
 "use strict";
 
+const { isDevelopment } = require("../config");
 const { CompilerHooksWebpackPlugin } = require("./CompilerHooksWebpackPlugin");
 const { invalidateSSRCache } = require("./invalidateSSRCache");
 
-const isDevelopment = process.env.NODE_ENV !== "production";
 const resolveExtensions = [".js", ".jsx", ".json", ".wasm"];
-
-exports.isDevelopment = isDevelopment;
 
 exports.defaultJsRule = {
   test: /\.jsx?$/,

commit f75348d38e1c2c9ccd6af0ab7234d75dbacf3181

chore: fix HMR broke by 62c9c09

webpack/webpack.csr.config.js
@@ -6,12 +6,8 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
-const {
-  isDevelopment,
-  defaultJsRule,
-  commonConfig,
-} = require("./webpack.common.config");
-const { CSR_PORT, SSR_PORT, LAZY_LOAD } = require("../config");
+const { defaultJsRule, commonConfig } = require("./webpack.common.config");
+const { CSR_PORT, SSR_PORT, LAZY_LOAD, isDevelopment } = require("../config");
 
 if (isDevelopment) {
   //enable SSR with fast-refresh